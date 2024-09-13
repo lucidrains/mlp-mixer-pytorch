0821b20653a3b1df347b92dc0a440b651f1876d5
Phil Wang <lucidrains@gmail.com>
2024-09-13 07:28:16 -0700

    fix another pypi thing

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
   version = '0.2.0',
   license='MIT',
   description = 'MLP Mixer - Pytorch',
+  long_description_content_type = 'text/markdown',
   author = 'Phil Wang',
   author_email = 'lucidrains@gmail.com',
   url = 'https://github.com/lucidrains/mlp-mixer-pytorch',
